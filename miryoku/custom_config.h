ef083d21a644e58e54d04098841f26013d1a1f35
Thomas Fritz <fritthom@amazon.at>
2022-12-03 14:27:03 +0100

    Changed custom config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,17 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// Copyright 2022 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+#define CONFIG_ZMK_DISPLAY_HIDE_MOMENTARY_LAYERS=y
+#define CONFIG_CUSTOM_WIDGET_LOGO_IMAGE_MIRYOKU=y
+#define CONFIG_IL0323_INVERT=y
+
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+//#define MIRYOKU_EXTRA_COLEMAKDH
+//#define MIRYOKU_NAV_INVERTEDT
+#define MIRYOKU_CLIPBOARD_MAC
+//#define MIRYOKU_LAYERS_FLIP
+//#define MIRYOKU_MAPPING_EXTENDED_THUMBS
