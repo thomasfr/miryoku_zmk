296189b0d5d9686bca93949907d02b6b102acef4
Thomas Fritz <fritthom@amazon.at>
2022-12-04 14:56:02 +0100

    Fixed kconfig

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,12 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-//#define CONFIG_ZMK_DISPLAY_HIDE_MOMENTARY_LAYERS=n
-#define CONFIG_CUSTOM_WIDGET_LOGO_IMAGE_MIRYOKU=y
-#define CONFIG_IL0323_INVERT=y
-
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
-#define CONFIG_ZMK_MOUSE=y
 
 #define MIRYOKU_ALPHAS_QWERTY
 #define MIRYOKU_TAP_QWERTY
