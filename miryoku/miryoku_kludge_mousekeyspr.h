5318393cbb34b02a4c6e75d2d68353e8550501f2
Thomas Fritz <fritthom@amazon.at>
2022-12-09 23:02:49 +0100

    changed mousemove speed

--- miryoku/miryoku_kludge_mousekeyspr.h
+++ miryoku/miryoku_kludge_mousekeyspr.h
@@ -5,8 +5,8 @@
 
 #include <dt-bindings/zmk/mouse.h>
 
-#define U_MOUSE_MOVE_MAX 2000
-#define U_MOUSE_MOVE_EXPONENT 3
+#define U_MOUSE_MOVE_MAX 1800
+#define U_MOUSE_MOVE_EXPONENT 2
 #define U_MOUSE_MOVE_TIME 1000
 #define U_MOUSE_MOVE_DELAY 0
 #define U_MOUSE_SCROLL_MAX 100
