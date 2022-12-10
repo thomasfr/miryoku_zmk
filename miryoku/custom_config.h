012266a60edaca36cba35239fd7b2eed59fc8c55
Thomas Fritz <fritthom@amazon.at>
2022-12-11 00:30:57 +0100

    added tap dance for layer toggle

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -53,7 +53,7 @@ MIRYOKU_X(ONEHAND,"Onehand")
 
 
 #define MIRYOKU_LAYER_BASE \
-&td_tog,           &kp W,            &kp E,             &kp R,             &kp T,             /***/         &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,                 \
+&td_tog Q U_ONEHAND,&kp W,            &kp E,             &kp R,             &kp T,             /***/         &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,                 \
 U_MT(LSHFT, A),    U_MT(LCTRL, S),    U_MT(LOPT, D),     U_MT(LCMD, F),     U_MT(LHYPER, G),   /***/         U_MT(RHYPER, H),   U_MT(RCMD, J),     U_MT(ROPT, K),     U_MT(RCTRL, L),    U_MT(RSHFT, SQT),      \
 U_LT(U_PLAIN, Z),  &kp X,             &kp C,             &kp V,             &kp B,             /***/         &kp N,             &kp M,             U_COMMA_SEMI,      U_DOT_COLON,       U_FSLH_BSLH,           \
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPC),  U_LT(U_MOUSE, TAB),/***/         U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
@@ -112,7 +112,7 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPC),  U_LT
 
 
 #define MIRYOKU_LAYER_ONEHAND \
-U_NA,              U_CUT,             U_CPY,             U_PST,             U_NA,             /***/         &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,                 \
+&tog U_ONEHAND,    U_CUT,             U_CPY,             U_PST,             U_NA,             /***/         &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,                 \
 U_NA,              &kp DEL,           &kp RET,           &kp BSPC,          U_NA,             /***/         &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,               \
 U_NA,              U_NA,              U_NA,              U_NA,              U_NA,             /***/         &kp N,             &kp M,             U_COMMA_SEMI,      U_DOT_COLON,       U_FSLH_BSLH,           \
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPC),  U_LT(U_MOUSE, TAB),/***/        U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
