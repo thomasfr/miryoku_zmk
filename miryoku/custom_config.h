6ff962c65208a9ff68aa11782aa2cfba8f7594cb
Thomas Fritz <fritthom@amazon.at>
2022-12-17 14:20:16 +0100

    trying to fix binding-cells

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -66,7 +66,7 @@ MIRYOKU_X(FUN,    "Fun") \
 #define MIRYOKU_LAYER_BASE \
 &tdto_q_switch,       &kp W,                &kp E,                &kp R,                &kp T,                /***/   &kp Y,                &kp U,                &kp I,                &kp O,                &kp P,                 \
 U_MT(LSHFT, A),       U_MT(LCTRL, S),       U_MT(LOPT, D),        U_MT(LCMD, F),        U_MT(LHYPER, G),      /***/   U_MT(RHYPER, H),      U_MT(RCMD, J),        U_MT(ROPT, K),        U_MT(RCTRL, L),       U_MT(RSHFT, SEMI),     \
-&tdto_z_plain,        &kp X,                &kp C,                TDSL(V),              &kp B,                /***/   &kp N,                TDSL(M),              &kp COMMA,            &kp DOT,              &kp FSLH,              \
+&tdto_z_plain,        &kp X,                &kp C,                &tdsl V,              &kp B,                /***/   &kp N,                &tdsl M,              &kp COMMA,            &kp DOT,              &kp FSLH,              \
 U_NP,                 U_NP,                 U_LT(U_MEDIA, ESC),   U_LT(U_NAV, SPC),     U_LT(U_MOUSE, TAB),   /***/   U_LT(U_SYM, RET),     U_LT(U_NUM, BSPC),    U_LT(U_FUN, DEL),     U_NP,                 U_NP
 
 
--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -5,4 +5,3 @@
 
 #define U_MT(MOD, TAP) &u_mt MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
-#define TDSL(TAP) &tdsl TAP
